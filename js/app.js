311ef7fae74ffdab4af406f7f60b8c71be025a7e
TheRedeemed <abdoulkaders77@gmail.com>
2014-05-30 22:51:29 -0400

    Shopping List v1.3

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
 $(document).ready(function(){
 	
 	// use enter to add list items
-	$(this).keyup(function(e){
+	$('#itemInput').keyup(function(e){
 		 var key = e.keyCode
 		if(key == 13) {
-			event.preventDefault();
+			//event.preventDefault();
 			addItem();
 		};
 	});	
-
+	
 	$('#addItem').click(function(){
 		addItem();
-	});
+	}); 
 
 });
 
@@ -21,7 +21,14 @@ function addItem(){
 	var upd = $('<div class="itemUpdate"></div>');
 	var comp = $('<div class="itemComplete"></div>');
 	var del = $('<div class="itemDelete"></div><br>');
-	var myItem = $('<div class="item"></div>').text(itemDesc);
+	var myItem = $('<input id="item" type="text" name="item" value="'+itemDesc+'">').prop('readonly', true);
+	var myItemEditable = $('<input id="item" type="text" name="item" value="'+itemDesc+'">').prop('readonly', false);
+	var updConfirm = $('<div class="updateValidate"></div>');
+	var updCancel = $('<div class="updateCancel"></div>');
+	 
+	//var myItemEditable = $('<input id="item" type="text" name="item" value="'+itemDesc+'">').prop('disabled', false);
+			   //$('<div class="item"></div>').text(itemDesc);
+			   //$("input").prop('disabled', true);
 
 		if(itemDesc == ""){
 			alert('Please enter an item before you can add it to the list');
@@ -30,23 +37,69 @@ function addItem(){
 			$(".shoppingList").append(myItem)
 			.append(upd)
 			.append(comp)
-			.append(del);
+			.append(del)
+			.append(myItemEditable)
+			.append(updConfirm)
+			.append(updCancel);
+			$(myItemEditable).hide();
+			$(updConfirm).hide();
+			$(updCancel).hide();
 			$('#itemInput').val("");
-			$('#itemInput').empty();
+			//$('#itemInput').empty();
 		}
-
-
+		
+		$(myItem).dblclick(function(){
+		});
+		
 		$(upd).click(function(){
-			alert('Updating '+ itemDesc);
+			$(myItem).hide();
+			$(upd).hide();
+			$(comp).hide();
+			$(del).hide();
+			$(myItemEditable).show();
+			$(updConfirm).show();
+			$(updCancel).show();
+			
+			
+			$(updConfirm).click(function(){
+
+				var updatedItemVal = $(myItemEditable).val();
+				//alert(updatedItemVal);
+
+				if(updatedItemVal == ""){
+					
+					alert('Please enter an item a new or edited value before you can save');
+
+				} else {
+
+					$(myItem).val(updatedItemVal).show();
+					$(upd).show();
+					$(comp).show();
+					$(del).show();
+					$(myItemEditable).hide();
+					$(updConfirm).hide();
+					$(updCancel).hide();					
+				}
+			});
+			
+			$(updCancel).click(function(){
+				$(myItem).show();
+				$(upd).show();
+				$(comp).show();
+				$(del).show();
+				$(myItemEditable).hide();
+				$(updConfirm).hide();
+				$(updCancel).hide();
+			});
+			
 		});
 
 		$(comp).click(function(){
-			alert('Marking ' + itemDesc + ' Complete ');
+			$(myItem).toggleClass('markComplete');
 		});
 
 
 		$(del).click(function(){
-			alert('Removing ' + itemDesc);
 			$(myItem).remove();
 			$(upd).remove();
 			$(comp).remove();
